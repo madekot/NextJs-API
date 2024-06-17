f819a56be4584fc12fe0945121060df37466d641
Mikhail Golubtsov <mgolubcov@gmail.com>
2024-06-17 14:02:47 +0300

    _

--- src/components/AllPosts.tsx
+++ src/components/AllPosts.tsx
@@ -1,42 +1,51 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useQuery } from 'react-query';
 import { Post } from '@prisma/client';
 
+const fetchPosts = async () => {
+    const res = await fetch('/api/posts');
+    if (!res.ok) {
+        throw new Error('Failed to fetch posts');
+    }
+    return res.json();
+};
+
+type Status = 'idle' | 'loading' | 'success' | 'error'
+
 export default function AllUsersAllPosts() {
-    const [posts, setPosts] = useState<(Post & { author: string })[]>([]);
-    const [error, setError] = useState('');
+    const [currentStatus, setCurrentStatus] = useState<Status>('idle');
+
+    const { data: posts, isLoading } = useQuery<(Post & { author: string })[]>('posts', fetchPosts, {
+        refetchInterval: 5000,
+        onSuccess: () => setCurrentStatus('success'),
+        onError: () => setCurrentStatus('error'),
+    });
 
     useEffect(() => {
-        fetch('/api/posts')
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error('Failed to fetch posts');
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setPosts(data);
-            })
-            .catch(() => {
-                setError('Failed to fetch posts');
-            });
-    }, []);
-
-    if (error) {
-        return <p>{error}</p>;
-    }
+        if (isLoading) {
+            setCurrentStatus('loading');
+        }
+    }, [isLoading]);
 
     return (
         <div>
             <h1>All Users Posts</h1>
-            <ul>
-                {posts.length && posts.map((post) => (
-                    <li key={post.id} style={{ margin: '15px 0' }}>
-                        <div>Author: {post.author}</div>
-                        <div>Title: {post.title}</div>
-                        <div>Content: {post.content}</div>
-                    </li>
-                ))}
-            </ul>
+            {{
+                idle: <p>idle...</p>,
+                loading: <p>Loading...</p>,
+                error: <p>Failed to fetch posts</p>,
+                success: (
+                    <ul>
+                        {posts?.length && posts.map((post) => (
+                            <li key={post.id} style={{ margin: '15px 0' }}>
+                                <div>Author: {post.author}</div>
+                                <div>Title: {post.title}</div>
+                                <div>Content: {post.content}</div>
+                            </li>
+                        ))}
+                    </ul>
+                ),
+            }[currentStatus]}
         </div>
     );
 }
